26e7e5f2004aff61afbef1cfe8d9f82e5f55c0ca
kbly538 <kubilay.bilgili@gmail.com>
2022-11-13 03:01:21 +0300

    initial commit
    Created project skeleton

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Resturant',
+            template: './src/index.html',
+        })
+    ],
+    devServer: { static: './dist' },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
